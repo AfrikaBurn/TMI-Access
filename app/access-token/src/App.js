4740cd8bd0506ca12852ddea90013a347cb21509
Scheepers <scheepers@afrikaburn.org>
2020-10-03 00:11:19 +0200

    Added app beginnings

---
+++ app/access-token/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Wrapper>
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ app/access-token/src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './header.css'
+
+/* TMI application header */
+class Header extends React.Component {
+
+  render(){
+    <header>
+      <UserProfileMenu></UserProfileMenu>
+      <AppSelector></AppSelector>
+    </header>
+  }
+
+}
+
+
+export default Header
